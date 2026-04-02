93dc17fc2890d9c83d7451f7e82c8011be4cb741
Varsharani <ranikasliwal7@gmail.com>
2026-04-02 16:54:36 +0530

    Add files via upload

---
+++ Prg1.py
@@ -0,0 +1,27 @@
+math = 50
+name = "Varsha"
+pi = 3.14
+result = True
+
+#To check data type of the variable
+print(type(math))
+print(type(name))
+print(type(pi))
+print(type(result))
+
+#To check address of the variable
+print(id(math))
+print(id(name))
+print(id(pi))
+print(id(result))
+
+# Output
+# <class 'int'>
+# <class 'str'>
+# <class 'float'>
+# <class 'bool'>
+# 140710180387480
+# 1927922584768
+# 1927919547472
+# 140710179490560
+
---
+++ Prg9.py
@@ -0,0 +1,8 @@
+h = float(input("Enter Height : ")) # Enter Height : 5
+
+inch = h * 12
+cm = inch * 2.54
+
+print("Value Of Inch : ",inch) # Value Of Inch :  60.0
+print("Value Of cm : ",cm) # Value Of cm :  152.4
+
