549807af26913a1ff5f44817d311f6a9f25a6bb2
Varsharani <ranikasliwal7@gmail.com>
2026-04-03 11:48:39 +0530

    Delete Prg9.py

--- Prg9.py
+++
@@ -1,8 +0,0 @@
-h = float(input("Enter Height : ")) # Enter Height : 5
-
-inch = h * 12
-cm = inch * 2.54
-
-print("Value Of Inch : ",inch) # Value Of Inch :  60.0
-print("Value Of cm : ",cm) # Value Of cm :  152.4
-
